4dedb4ceae820b0673717c4bf69ac007be54f011
Pallav Rastogi <72249956+PallavRastogi165@users.noreply.github.com>
2023-11-30 15:57:22 +0530

    added comments

--- blah.py
+++ blah.py
@@ -1,12 +1,14 @@
 import cv2
 
-
-
 image1 = cv2.imread(r"monarch.jpg")
 image2 = cv2.imread(r"enstin.jpg")
 
 gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+#a
+
+
+#enw commensts
 
 canny1 = cv2.Canny(gray1, 150, 200)
 canny2 = cv2.Canny(gray2, 100, 170)
@@ -14,3 +16,5 @@ canny2 = cv2.Canny(gray2, 100, 170)
 cv2.imshow("Canny Edge - Image 1", canny1)
 cv2.imshow("Canny Edge - Image 2", canny2)
 cv2.waitKey(0)
+
+print("hello")
